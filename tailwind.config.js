1f49ff0105ece1dee65533a0395aa664fcb6400e
viponcio <vitoria.poncio@gmail.com>
2024-07-26 19:44:10 -0300

    inicio do design resposivo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-gray': '#b3b3b3' // Customize this color as needed
+      }
+    }
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities(
+        {
+          '.scrollbar-gray': {
+            '&::-webkit-scrollbar': {
+              width: '12px'
+            },
+            '&::-webkit-scrollbar-thumb': {
+              background: '#b3b3b3',
+              borderRadius: '9999px'
+            },
+            '&::-webkit-scrollbar-track': {
+              background: '#e0e0e0',
+              borderRadius: '9999px'
+            }
+          }
+        },
+        ['responsive']
+      )
+    }
+  ]
 }
-
